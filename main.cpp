2f51fbab6510549d0133bda61c5dc5b0dea8507a
pu240802 <83825265+pu240802@users.noreply.github.com>
2021-10-31 22:04:50 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<string.h>
+#include "Sinhvien.h"
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	Sinhvien a[100];
+List l;
+l.menu();
+
+///Show(l);
+//KhoitaoList(l);
+	
+//	s= new Sinhvien[n];
+	
+	
+	
+
+	system("pause");
+	return 0;
+}
